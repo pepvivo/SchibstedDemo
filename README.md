d830a2627cbcee6da7bc01009914905b2bfd0cad
Pep Vivó <pepvivo@hotmail.com>
2019-05-24 14:16:24 +0200

    Add Help Page and a WebApitestClient for run test directly

--- README.md
+++ README.md
@@ -52,6 +52,7 @@ For the correct installation of the solution we advise, once the project is open
 - Attributes of authorization of classes for the control of permissions through roles.This application has been done thinking about encapsulation and reuse of code.Any new controller that has the attribute **AuthorizedRolesAttribute** will pass the user role control.
 - SessionBaseController is the base class of session control. Any new class that inherits from **SessionBaseController** at the moment that there is no or the session has been lost will be directed to the Login page.
 - Help page for web api controller (User). Additionally, the **WebApiTestClient** library has been included for direct testing of this api rest.
+-- You can find this help page in --/Help-- route of application url execution (Local execution: http://localhost:57001/help )
 - This application has been created so that simply including more roles in the list of roles the application will show and work with them without having to modify anything else (Dynamic execution).
 - Use of string resources (application ready for change the language easierly.
 - Internally and like demostration mode, controllers calls are made either directly or by Http requests.
--- WebApplication/Controllers/api/UsersController.cs
+++ WebApplication/Controllers/api/UsersController.cs
@@ -15,7 +15,9 @@ using WebApplication.Modules.Interfaces;
 namespace WebApplication.Controllers
 
 {
-
+    /// <summary>
+    /// Controller for user resources
+    /// </summary>
     [Authorize]
     [RoutePrefix("api/users")]
     public class UsersController : ApiController, IUsersController
@@ -24,6 +26,9 @@ namespace WebApplication.Controllers
         private Autheniication _autheniication;
 
 
+        /// <summary>
+        /// Default constructor
+        /// </summary>
         public UsersController()
         {
             _userManager = new UserManager(); 
@@ -31,6 +36,12 @@ namespace WebApplication.Controllers
 
         }
 
+        /// <summary>
+        /// Dependecy Inyection constructor
+        /// For testing purposes
+        /// </summary>
+        /// <param name="userManager">UserManager class to use</param>
+        /// <param name="autheniication">Authorizatin class to use</param>
         public UsersController(UserManager userManager, Autheniication autheniication) : base()
         {
             _userManager = userManager;
@@ -38,6 +49,10 @@ namespace WebApplication.Controllers
         }
 
         // GET api/users
+        /// <summary>
+        /// Get a List of users
+        /// </summary>
+        /// <returns>IHttpActionResult especifiquing the result of operation and a list of users</returns>
         [HttpGet]
         [Route("")]
         [ResponseType(typeof(IEnumerable<User>))]
@@ -62,6 +77,11 @@ namespace WebApplication.Controllers
         }
 
         // GET api/users/{id}
+        /// <summary>
+        /// Get a user
+        /// </summary>
+        /// <param name="id">Name identificator of user</param>
+        /// <returns>IHttpActionResult especifiquing the result of operation and a user</returns>
         [HttpGet]
         [Route("{id}")]
         [ResponseType(typeof(User))]
@@ -87,6 +107,11 @@ namespace WebApplication.Controllers
         }
 
         // POST api/users
+        /// <summary>
+        /// Add a new user
+        /// </summary>
+        /// <param name="value">Json string with user serialized</param>
+        /// <returns>IHttpActionResult especifiquing the result of operation</returns>
         [HttpPost]
         [Route("")]
         public virtual IHttpActionResult Post([FromBody]string value)
@@ -109,6 +134,11 @@ namespace WebApplication.Controllers
         }
 
         // PUT api/users
+        /// <summary>
+        /// Udate an user
+        /// </summary>
+        /// <param name="value">Json string with user serialized</param>
+        /// <returns>IHttpActionResult especifiquing the result of operation</returns>
         [Route("")]
         [HttpPut]
         public virtual IHttpActionResult Put([FromBody]string value)
@@ -131,19 +161,24 @@ namespace WebApplication.Controllers
         }
 
         // DELETE api/users/{id}
+        /// <summary>
+        /// Delete an user
+        /// </summary>
+        /// <param name="value">Name identificator of user</param>
+        /// <returns>IHttpActionResult especifiquing the result of operation</returns>
         [Route("{id}")]
         [HttpDelete]
-        public virtual IHttpActionResult Delete(string id)
+        public virtual IHttpActionResult Delete(string value)
         {
             if (!_autheniication.IsAuthentificated(User))
                 return Unauthorized();
 
             try
             {
-                if (_userManager.IsLastAdminUser(id))
+                if (_userManager.IsLastAdminUser(value))
                     return BadRequest("Last admin user cannot be deleted.");
 
-                return (_userManager.Delete(id)) ?
+                return (_userManager.Delete(value)) ?
                     (IHttpActionResult)Ok() :
                     (IHttpActionResult)NotFound();
             }
